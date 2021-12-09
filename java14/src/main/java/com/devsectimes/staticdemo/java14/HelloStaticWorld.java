0b34144d22b424ed39820d5fa6c7cee304136539
Steve Poole <spoole167@googlemail.com>
2021-12-09 14:55:58 +0000

    added intermedial java levels

---
+++ java14/src/main/java/com/devsectimes/staticdemo/java14/HelloStaticWorld.java
@@ -0,0 +1,36 @@
+package com.devsectimes.staticdemo.java14;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+public class HelloStaticWorld {
+
+  public static void main(String[] args ) {
+
+      System.out.println("Hello Static World (Java 15 style)");
+      HelloStaticWorld h=new HelloStaticWorld();
+      System.out.println("Total ="+h.add(args));
+
+  }
+
+public int add(String... args) {
+
+    List<Integer> arglist = new ArrayList<>();
+
+    for(int i=0;i<args.length;i++) {
+      arglist.add(Integer.parseInt(args[i]));
+    }
+
+    int total=0;
+
+    Iterator<Integer> i=arglist.iterator();
+
+    while(i.hasNext()) {
+      total+=i.next();
+    }
+
+    return total;
+  }
+
+}
---
+++ java15/src/test/java/com/devsectimes/staticdemo/java15/TestHelloStaticWorld.java
@@ -0,0 +1,19 @@
+package com.devsectimes.staticdemo.java15;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestHelloStaticWorld {
+
+  @Test
+  public void testSum() {
+
+      HelloStaticWorld h=new HelloStaticWorld();
+      int result=h.add("1","2","3","4","5");
+
+      assertEquals(15,result);
+
+  }
+
+}
